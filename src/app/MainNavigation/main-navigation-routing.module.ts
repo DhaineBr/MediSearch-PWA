8d25721b0704ddf83c7d0d90484f7b340f6b15c5
Dhaine-Leader <dhaine_br@protonmail.com>
2023-10-01 23:29:49 +0800

    October 1 UI update v2

--- src/app/MainNavigation/main-navigation-routing.module.ts
+++ src/app/MainNavigation/main-navigation-routing.module.ts
@@ -2,13 +2,19 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainNavigationComponent } from './main-navigation.component';
 import { HomeComponent } from './home/home.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { AccountComponent } from './account/account.component';
+import { SearchComponent } from './search/search.component';
 
 
 const routes: Routes = [
   {path: 'home', 
     component: MainNavigationComponent, 
   children:  [
-    {path: 'dashboard', component: HomeComponent}
+    {path: 'landing', component: HomeComponent},
+    {path: 'search', component: SearchComponent},
+    {path: 'reservation', component: ReservationComponent},
+    {path: 'account', component: AccountComponent},
   ]},
   
 ];
--- src/app/MainNavigation/main-navigation.module.ts
+++ src/app/MainNavigation/main-navigation.module.ts
@@ -5,10 +5,18 @@ import { MainNavigationRoutingModule } from './main-navigation-routing.module';
 import { MainNavigationComponent } from './main-navigation.component';
 
 import { FormsModule } from '@angular/forms';
+import { AccountComponent } from './account/account.component';
+import { SearchComponent } from './search/search.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    MainNavigationComponent
+    MainNavigationComponent,
+    AccountComponent,
+    SearchComponent,
+    HomeComponent,
+    ReservationComponent
   ],
   imports: [
     CommonModule,
