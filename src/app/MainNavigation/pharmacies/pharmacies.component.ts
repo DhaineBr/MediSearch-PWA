b8273475db4b126d17f5c416fe9cca578bc2ae43
Dhaine-Leader <dhaine_br@protonmail.com>
2023-10-24 21:13:30 +0800

    October 24 update, dummy data and sort functions

--- src/app/MainNavigation/pharmacies/pharmacies.component.ts
+++ src/app/MainNavigation/pharmacies/pharmacies.component.ts
@@ -1,21 +1,93 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+interface Store {
+  storeName: string;
+  storeAddress: string;
+  storeHours: string;
+  storeRating: number;
+}
 
 @Component({
   selector: 'app-pharmacies',
   templateUrl: './pharmacies.component.html',
   styleUrls: ['./pharmacies.component.scss']
 })
-export class PharmaciesComponent {
+
+export class PharmaciesComponent implements OnInit{
+  @ViewChild('button2', { read: ElementRef }) button2!: ElementRef;
+  @ViewChild('button3', { read: ElementRef }) button3!: ElementRef;
+  @ViewChild('button4', { read: ElementRef }) button4!: ElementRef;
+
+  stores: Store[] = [];
+  sortedStores: Store[] = [];
+
+
+  ngOnInit(): void {
+    this.stores = this.generateDummyData(10);
+  }
+
+
+  generateDummyData(count: number): Store[] {
+    const dummyStore: Store[] = [];
+
+    for (let i = 1; i <= count; i++ ) {
+      const store: Store = {
+        storeName: `Pharmacy ` + i,
+        storeAddress: `Brgy. ` + i,
+        storeHours: `8:00 AM - 8:00 PM`,
+        storeRating: 4.03 + (i/20),
+      };
+
+      dummyStore.push(store);
+    }
+    return dummyStore;
+  }
+
   activeButton: HTMLButtonElement | null = null;
 
   toggleButtonActive(event: MouseEvent, button: HTMLButtonElement) {
-    // Deactivate the previously active button
-    if (this.activeButton) {
-      this.activeButton.classList.remove('active');
+    if (this.activeButton === button) {
+
+      button.classList.remove('active');
+      this.activeButton = null;
+    } else {
+
+      if (this.activeButton) {
+        this.activeButton.classList.remove('active');
+      }
+
+      button.classList.add('active');
+      this.activeButton = button;
     }
 
-    // Activate the clicked button
-    button.classList.add('active');
-    this.activeButton = button;
+
+    if (button === this.button2.nativeElement) {
+      this.sortByRating();
+    }
+
+    if (button === this.button3.nativeElement) {
+      this.sortByName();
+    }
+
+    if (button === this.button4.nativeElement) {
+      this.sortByNameReverse();
+    }
+
+
+
   }
+
+  sortByRating() {
+    this.stores.sort((a, b) => b.storeRating - a.storeRating);
+  }
+
+  sortByName()  {
+    this.stores.sort((a, b) => a.storeName.localeCompare(b.storeName));
+  }
+
+  sortByNameReverse() {
+    this.stores.sort((a, b) => b.storeName.localeCompare(a.storeName));
+  }
+
 }
+
--- src/app/MainNavigation/reservation/reservation.component.ts
+++ src/app/MainNavigation/reservation/reservation.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Reservation {
+  orderID: string;
+  storeName: string;
+  storeAddress: string;
+  validityPeriod: Date;
+}
 
 @Component({
   selector: 'app-reservation',
   templateUrl: './reservation.component.html',
   styleUrls: ['./reservation.component.scss']
 })
-export class ReservationComponent {
+export class ReservationComponent implements OnInit {
+  reservations: Reservation[] = [];
 
+  ngOnInit(): void {
+    this.reservations = this.generateDummyData(10);
+  }
+
+  generateDummyData(count: number): Reservation[] {
+    const dummyReservation: Reservation[] = [];
+
+    for (let i = 1; i <= count; i++) {
+      const validityPeriod = new Date('2023-10-24');
+      validityPeriod.setDate(validityPeriod.getDate() + i);
+
+      const reservation: Reservation = {
+        orderID: `932105321${i}`,
+        storeName: `Pharmacy ${i}`,
+        storeAddress: `Brgy. ${i}`,
+        validityPeriod: validityPeriod,
+      };
+
+      dummyReservation.push(reservation);
+    }
+    return dummyReservation;
+  }
 }
