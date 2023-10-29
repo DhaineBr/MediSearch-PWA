423474455fd2f8454155b8debf525369bdf1cbef
Dhaine-Leader <dhaine_br@protonmail.com>
2023-10-29 20:56:02 +0800

    October 29 update

--- src/app/MainNavigation/main-navigation.module.ts
+++ src/app/MainNavigation/main-navigation.module.ts
@@ -10,12 +10,7 @@ import { AccountComponent } from './account/account.component';
 import { ReservationComponent } from './reservation/reservation.component';
 import { HomeComponent } from './home/home.component';
 import { PharmaciesComponent } from './pharmacies/pharmacies.component';
-import { AccountInfoComponent } from './account/account-info/account-info.component';
-import { AddressesComponent } from './account/addresses/addresses.component';
-import { HistoryComponent } from './account/history/history.component';
-import { FollowingComponent } from './account/following/following.component';
-import { ReviewsComponent } from './account/reviews/reviews.component';
-import { AccountModule } from './account/account.module';
+
 
 @NgModule({
   declarations: [
@@ -29,7 +24,7 @@ import { AccountModule } from './account/account.module';
     CommonModule,
     MainNavigationRoutingModule,
     FormsModule,
-    AccountModule
+
 
   ]
 })
