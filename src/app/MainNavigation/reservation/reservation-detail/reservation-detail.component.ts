d3e7a96cabeab596165c10cc294c9043c16def02
Dhaine-Leader <dhaine_br@protonmail.com>
2023-11-04 17:26:14 +0800

    Map update

--- src/app/MainNavigation/reservation/reservation-detail/reservation-detail.component.ts
+++ src/app/MainNavigation/reservation/reservation-detail/reservation-detail.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-reservation-detail',
   templateUrl: './reservation-detail.component.html',
   styleUrls: ['./reservation-detail.component.scss']
 })
-export class ReservationDetailComponent {
+export class ReservationDetailComponent implements OnInit {
+  private map!: L.Map;
 
+  ngOnInit(): void {
+    this.initializeMap();
+  }
+
+  private initializeMap(): void {
+    this.map = L.map('map1').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+
+    L.marker([51.5, -0.09]).addTo(this.map)
+      .bindPopup('Hello, this is a marker!')
+      .openPopup();
+  }
 }
